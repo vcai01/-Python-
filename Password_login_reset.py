fbadf01ffb468953e72a7737023b69fc941756eb
vcai01 <49544931+vcai01@users.noreply.github.com>
2019-06-30 11:34:47 -0700

    Create Password_login_reset.py

---
+++ Password_login_reset.py
@@ -0,0 +1,24 @@
+"""
+Password login and reset
+note:
+    1. the application of xlist[-1]
+    2. xlist.append(word)
+"""
+
+password_list = ['*#*#', '12345']
+def account_login():
+    password = input('Password: ')
+    password_correct = password == password_list[-1]
+    password_reset = password == password_list[0]
+    if password_correct:
+        print('Login success!')
+    elif password_reset:
+        new_password = input('Enter a new password: ')
+        password_list.append(new_password)
+        print('Your password has changed successfully!')
+        account_login()
+    else:
+        print('Wrong password or invalid input!')
+        account_login()
+
+account_login()
